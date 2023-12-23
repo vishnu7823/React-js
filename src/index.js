c085829b3c5182e9bcf22154644fea29ed3a946e
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-23 21:49:42 +0530

    reactjs hello

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ 
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+   < React.StrictMode>
+   <h1>
+    hello
+   </h1>
+
+   </React.StrictMode>
+);
+
+
+
+
+
