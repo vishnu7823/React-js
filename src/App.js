e8fa9384f1f0045f82e629de30787a2e352eaa88
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-24 21:39:44 +0530

    basic component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+ const app=()=>{
+    return (
+        <h1>Hello!</h1>
+    );
+}
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
- 
-const root=ReactDOM.createRoot(document.getElementById("root"))
+import App from "./App";
+import "./index.css"
+const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   < React.StrictMode>
-   <h1>
-    hello
-   </h1>
-
-   </React.StrictMode>
+    <React.StrictMode>
+        <App /> 
+    </React.StrictMode>
 );
-
-
-
-
-
